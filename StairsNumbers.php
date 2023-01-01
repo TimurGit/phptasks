bbd065c7a16f8f9345bcbd2553a082b16f3b70ef
Timur Mustafin <mustafintf@eruditor-group.com>
2023-01-01 22:20:35 +0500

    tests for StairNumbers.php

--- StairsNumbers.php
+++ StairsNumbers.php
@@ -1,17 +1,22 @@
 <?php
 
-$i               = 1;
-$line_number     = 1;
-$numbers_in_line = 1;
-while ($i < 100) {
-  echo $i;
-  if ($line_number === $numbers_in_line) {
-    $numbers_in_line = 1;
-    $line_number++;
-    echo PHP_EOL;
-  } else {
-    echo ' ';
-    $numbers_in_line++;
+function stairs_numbers(){
+  $result = '';
+  $i               = 1;
+  $line_number     = 1;
+  $numbers_in_line = 1;
+  while ($i < 100) {
+    $result .= $i;
+    if ($line_number === $numbers_in_line) {
+      $numbers_in_line = 1;
+      $line_number++;
+      $result .= PHP_EOL;
+    } else {
+      $result .= ' ';
+      $numbers_in_line++;
+    }
+    $i++;
   }
-  $i++;
+  $result = substr($result,0,-1);
+  return $result;
 }
